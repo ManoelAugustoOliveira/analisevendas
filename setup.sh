b0a99c731bfbf93fc17ce9d217274e915e20c32a
atualização <manoelaugusto973@gmail.com>
2022-08-15 14:07:38 -0400

    terceiro commit

--- setup.sh
+++ setup.sh
@@ -4,10 +4,3 @@ echo "\
 [general]\n\
 email = \"manoelaugusto973#gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
